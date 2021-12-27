3fda7f9e154b277827fb5c929f47d4ea7dd010c1
Rodrigo Moraes <78232612+RodrigoMoraes28@users.noreply.github.com>
2021-12-26 23:55:45 -0300

    Update Fone.java

--- Exercicos_de_POO/Semana6/Contato - Agenda 1/Fone.java
+++ Exercicos_de_POO/Semana6/Contato - Agenda 1/Fone.java
@@ -3,7 +3,7 @@ class Fone {
     private String number;
     public Fone(String id, String number);
     //verifica se o número é um número de telefone válido
-    //acho que mandei o arquivo errado
+    //acho que mandei o arquivo errado   
     public static boolean validate(String number);
     //O resultado deve ficar assim
     //oi:1234
