6eb81c8413930af3c1c3a25bef0c2b374aa7d4cb
Rodrigo Moraes <78232612+RodrigoMoraes28@users.noreply.github.com>
2021-12-31 17:26:35 -0300

    Add files via upload

---
+++ Exercicos_de_POO/Semana6/Agenda1/Fone.java
@@ -0,0 +1,48 @@
+public class Fone {
+    private String id;
+    private String number;
+
+    public Fone(String id, String number){
+        this.id = id;
+        this.number = number;
+    }
+    
+    //verifica se o número é um número de telefone válido
+    public static boolean validate(String number){
+        boolean ehigual = false;
+        for(int j = 0 ; j < number.length(); j++)
+            if((number.charAt(j) >= 48 && number.charAt(j) <= 57)
+            || (number.charAt(j) == 40 || number.charAt(j) == 41 || number.charAt(j) == 46))
+            ehigual = true;
+        else{
+            ehigual = false;
+            break;
+        }
+        return ehigual;
+    }
+    //O resultado deve ficar assim
+    //oi:1234
+    public String toString(){
+        return(id + ":" + number);
+    }
+    //GETS e SETS
+    public String getId(){
+        return this.id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public String getNumber(){
+        return this.number;
+    }
+
+    public void setNumber(String number){
+        this.number = number;
+    }
+    //utiliza o static validate para retornar se essa instancia do fone é valida
+    public boolean isValid(){
+        return validate(number);
+    }
+}
+
+
